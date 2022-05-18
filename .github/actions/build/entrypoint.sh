927b53b07ad077ba6edaf71a7000f1dabba701e2
usimd <11619247+usimd@users.noreply.github.com>
2022-05-18 13:50:53 +0200

    Fix shellcheck issues and symlinking

--- .github/actions/build/entrypoint.sh
+++ .github/actions/build/entrypoint.sh
@@ -11,8 +11,8 @@ FINAL_STAGE_REACHED=0
 for stage in ${!PI_GEN_STAGES[@]}; do
   if [[ "$FINAL_STAGE_REACHED" == "1" ]]; then
     echo "Skipping pi-gen ${PI_GEN_STAGES[$stage]}"
-    touch ${PI_GEN_DIR}/${PI_GEN_STAGES[$stage]}/SKIP
-    touch ${PI_GEN_DIR}/${PI_GEN_STAGES[$stage]}/SKIP_IMAGES
+    touch "${PI_GEN_DIR}/${PI_GEN_STAGES[$stage]}/SKIP"
+    touch "${PI_GEN_DIR}/${PI_GEN_STAGES[$stage]}/SKIP_IMAGES"
   fi
 
   if [[ "${PI_GEN_STAGES[$stage]}" == "$TARGET_STAGE" ]]; then
@@ -20,6 +20,8 @@ for stage in ${!PI_GEN_STAGES[@]}; do
   fi
 done
 
-ln -s ${TARGET_STAGE} ${PI_GEN_DIR}
+if [[ " ${PI_GEN_STAGES[*]} " !~ " ${TARGET_STAGE} " ]]; then
+  ln -s ${TARGET_STAGE} ${PI_GEN_DIR}
+fi
 
 ${PI_GEN_DIR}/build.sh -c $CONFIG_FILE
