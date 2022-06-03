14af3616d6ba4dc577d62b5f4dc4896e4d856b68
usimd <11619247+usimd@users.noreply.github.com>
2022-06-03 23:32:29 +0200

    Absolutize stage paths

--- common/test/pi-gen-config.test.ts
+++ common/test/pi-gen-config.test.ts
@@ -1,15 +1,6 @@
-import {
-  PiGenConfig,
-  DEFAULT_CONFIG,
-  writeToFile,
-  loadFromFile
-} from '../src/pi-gen-config'
-import {writeFile, readFile} from 'fs/promises'
-
-jest.mock('fs/promises', () => ({
-  writeFile: jest.fn(),
-  readFile: jest.fn((file: string, options: object) => {})
-}))
+import {PiGenConfig, writeToFile, loadFromFile} from '../src/pi-gen-config'
+import fs from 'fs/promises'
+import * as path from 'path'
 
 describe('PiGenConfig', () => {
   it('instance should write config to file', async () => {
@@ -19,13 +10,20 @@ describe('PiGenConfig', () => {
     config.firstUserName = 'test'
     config.stageList = 'stage0 stage1'
 
+    jest.spyOn(fs, 'writeFile').mockImplementation(() => Promise.resolve())
+    jest
+      .spyOn(fs, 'realpath')
+      .mockImplementation(p =>
+        Promise.resolve(`/pi-gen/${path.basename(p.toString())}`)
+      )
+
     let fileName = 'test-file'
-    await writeToFile(config, fileName)
-    expect(writeFile).toHaveBeenCalledWith(
+    await writeToFile(config, 'pi-gen', fileName)
+    expect(fs.writeFile).toHaveBeenCalledWith(
       fileName,
       expect.stringMatching(
         new RegExp(
-          `^(?=.*IMG_NAME="${imageName}"$)(?=.*STAGE_LIST="stage0 stage1"$)(?=.*FIRST_USER_NAME="test"$).*`,
+          `^(?=.*IMG_NAME="${imageName}"$)(?=.*STAGE_LIST="/pi-gen/stage0 /pi-gen/stage1"$)(?=.*FIRST_USER_NAME="test"$).*`,
           'sm'
         )
       )
@@ -33,9 +31,11 @@ describe('PiGenConfig', () => {
   })
 
   it('instance loads values correctly from file', async () => {
-    ;(readFile as jest.Mock).mockImplementationOnce(async () =>
-      Promise.resolve('IMG_NAME="test"\nCOMPRESSION_LEVEL="8"')
-    )
+    jest
+      .spyOn(fs, 'readFile')
+      .mockImplementationOnce(() =>
+        Promise.resolve('IMG_NAME="test"\nCOMPRESSION_LEVEL="8"')
+      )
     const piGen = await loadFromFile('any')
 
     expect(piGen).toMatchObject({imgName: 'test', compressionLevel: '8'})
