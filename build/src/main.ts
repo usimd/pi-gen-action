af737b4692fe62b9bf5b43f16abee89935632360
Simon Domke <simon.domke@bmw.de>
2022-06-07 23:04:44 +0200

    Add group outputs and custom stage test

--- build/src/main.ts
+++ build/src/main.ts
@@ -2,12 +2,20 @@ import * as core from '@actions/core'
 import {PiGen} from '@pi-gen-action/common'
 
 async function run(): Promise<void> {
-  const configFile = core.getInput('config-file')
-  const pigenDir = core.getInput('pi-gen-dir')
-  const verbose = core.getBooleanInput('verbose-output')
+  try {
+    core.startGroup('Running pi-gen build')
 
-  const piGen = new PiGen(pigenDir, configFile)
-  await piGen.build(verbose)
+    const configFile = core.getInput('config-file')
+    const pigenDir = core.getInput('pi-gen-dir')
+    const verbose = core.getBooleanInput('verbose-output')
+
+    const piGen = new PiGen(pigenDir, configFile)
+    await piGen.build(verbose)
+  } catch (error) {
+    if (error instanceof Error) core.setFailed(error.message)
+  } finally {
+    core.endGroup()
+  }
 }
 
 run()
