132e5e6bcae19a14bf8ce26cc73691c01445577e
usimd <11619247+usimd@users.noreply.github.com>
2022-06-29 13:10:42 +0200

    Enable user to use a forked pi-gen repo

--- __test__/clone-pigen.test.ts
+++ __test__/clone-pigen.test.ts
@@ -1,25 +1,49 @@
 import * as core from '@actions/core'
+import * as github from '@actions/github'
+import {GitHub} from '@actions/github/lib/utils'
 import {clonePigen} from '../src/clone-pigen'
 import {Git} from '../src/git'
-import {mock} from 'jest-mock-extended'
+import {mock, mockDeep} from 'jest-mock-extended'
 
 describe('Cloning pi-gen repo', () => {
-  it('fetches pi-gen repository', async () => {
+  it.each([
+    ['RPi-Distro/pi-gen', false, true],
+    ['owner/forked-repo', true, true],
+    ['another-owner/no-fork', false, false]
+  ])('fetches pi-gen repository %s', async (repo, isFork, succeed) => {
     const verbose = true
     const token = '123456'
     const ref = 'main'
     const targetDir = 'pi-gen-dir'
     const gitMock = mock<Git>()
+    const githubMock = mockDeep<InstanceType<typeof GitHub>>()
     jest.spyOn(core, 'getBooleanInput').mockReturnValue(verbose)
     jest.spyOn(core, 'getInput').mockReturnValue(token)
+    jest.spyOn(github, 'getOctokit').mockReturnValueOnce(githubMock)
     Git.getInstance = jest.fn().mockResolvedValue(gitMock)
+    githubMock.rest.repos.get.mockResolvedValueOnce({
+      data: {
+        fork: isFork,
+        source: {full_name: isFork ? 'RPi-Distro/pi-gen' : repo}
+      }
+    } as any)
 
-    await clonePigen(targetDir, ref)
+    const clone = clonePigen(repo, targetDir, ref)
 
-    expect(Git.getInstance).toBeCalledWith(targetDir, token, verbose)
-    expect(gitMock.clone).toBeCalledWith(
-      'https://github.com/RPi-Distro/pi-gen',
-      ref
-    )
+    if (!succeed) {
+      await expect(clone).rejects.toThrowError()
+    } else {
+      await expect(clone).resolves.not.toThrowError()
+    }
+
+    expect(githubMock.rest.repos.get).toBeCalledWith({
+      owner: repo.split('/')[0],
+      repo: repo.split('/')[1]
+    })
+
+    if (succeed) {
+      expect(Git.getInstance).toBeCalledWith(targetDir, token, verbose)
+      expect(gitMock.clone).toBeCalledWith(`https://github.com/${repo}`, ref)
+    }
   })
 })
--- src/clone-pigen.ts
+++ src/clone-pigen.ts
@@ -1,17 +1,37 @@
 import * as core from '@actions/core'
+import * as github from '@actions/github'
 import {Git} from './git'
 
 export async function clonePigen(
+  repo: string,
   piGenDirectory: string,
   ref: string
 ): Promise<void> {
   try {
+    const originalPiGenRepo = 'RPi-Distro/pi-gen'
+
     core.startGroup('Cloning pi-gen repository')
-    core.debug(`Checking ref ${ref} into ${piGenDirectory}`)
-    const verbose = core.getBooleanInput('verbose-output')
     const token = core.getInput('github-token')
+
+    const octokit = github.getOctokit(token)
+    const [owner, repoName] = repo.split('/')
+    const repoInfo = await octokit.rest.repos.get({
+      owner: owner,
+      repo: repoName
+    })
+
+    if (
+      repo !== originalPiGenRepo &&
+      (!repoInfo.data.fork ||
+        repoInfo.data.source?.full_name !== originalPiGenRepo)
+    ) {
+      throw new Error(`${repo} is not a fork from ${originalPiGenRepo}`)
+    }
+
+    core.debug(`Checking out ref ${ref} into ${piGenDirectory}`)
+    const verbose = core.getBooleanInput('verbose-output')
     const git = await Git.getInstance(piGenDirectory, token, verbose)
-    await git.clone('https://github.com/RPi-Distro/pi-gen', ref)
+    await git.clone(`https://github.com/${repo}`, ref)
   } finally {
     core.endGroup()
   }
