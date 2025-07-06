c37ca6bf5375fbb355f1ecdd00a6be2977b9fc8d
usimd <11619247+usimd@users.noreply.github.com>
2025-07-06 22:20:52 +0200

    Add TEMP_REPO support

--- __test__/pi-gen-config.test.ts
+++ __test__/pi-gen-config.test.ts
@@ -146,6 +146,11 @@ describe('PiGenConfig', () => {
       'aptProxy',
       'this/is/not/valid',
       'apt-proxy is not a valid URL. Make it point to a correct http/https address'
+    ],
+    [
+      'tempRepo',
+      'deb s3://trying-a-bucket-as-source-for-once',
+      'temp-repo contains an invalid one-line-style format. See readme for details. Offending line: deb s3://trying-a-bucket-as-source-for-once'
     ]
   ])(
     'rejects %s with invalid value %s',
@@ -190,6 +195,20 @@ describe('PiGenConfig', () => {
         ssh-dss AAAAB3NzaC1kc3MAAAEBAI95Ndm5qum/q+2Ies9JUbbzLsWeO683GOjqxJYfPv02BudDUanEGDM5uAnnwq4cU5unR1uF0BGtuLR5h3VJhGlcrA6PFLM2CCiiL/onEQo9YqmTRTQJoP5pbEZY+EvdIIGcNwmgEFexla3NACM9ulSEtikfnWSO+INEhneXnOwEtDSmrC516Zhd4j2wKS/BEYyf+p2BgeczjbeStzDXueNJWS9oCZhyFTkV6j1ri0ZTxjNFj4A7MqTC4PJykCVuTj+KOwg4ocRQ5OGMGimjfd9eoUPeS2b/BJA+1c8WI+FY1IfGCOl/IRzYHcojy244B2X4IuNCvkhMBXY5OWAc1mcAAAAdALr2lqaFePff3uf6Z8l3x4XvMrIzuuWAwLzVaV0AAAEAFqZcWCBIUHBOdQKjl1cEDTTaOjR4wVTU5KXALSQu4E+W5h5L0JBKvayPN+6x4J8xgtI8kEPLZC+IAEFg7fnKCbMgdqecMqYn8kc+kYebosTnRL0ggVRMtVuALDaNH6g+1InpTg+gaI4yQopceMR4xo0FJ7ccmjq7CwvhLERoljnn08502xAaZaorh/ZMaCbbPscvS1WZg0u07bAvfJDppJbTpV1TW+v8RdT2GfY/Pe27hzklwvIk4HcxKW2oh+weR0j4fvtf3rdUhDFrIjLe5VPdrwIRKw0fAtowlzIk/ieu2oudSyki2bqL457Z4QOmPFKBC8aIt+LtQxbh7xfb3gAAAQAG2DjHpzzWGYtVLzMRfXwRFmVNwOO1Rg7ZmLjcy0hWy2b2JzeYJJSj+mRa/GC/Si3e16b0nBJGWU6FcTGSzPOdU3xrMJGLqtIlnUyqS5UJf75xs7zJamuSJ/QMLsvzqglaFBygL5Iuc5KF8lluXFK9h4ggCYxJp2UhgpsX6QMAl7ITeTHFdGWs/nwBHafEwxY3DViTmrj7wXuz8QBzzh65+lIrbOnibg3gliBg77bFLfVEFdylu3f5R18c8sZ9U0c4DOA+ZGlmAqSHZOQtyf8p8T+yKbMBJaQ/R+y0qG/R5Ai1d/aBcGZ1W5b/BU9Z+6yb7ITGowGzObBhU3L4g22e test@example.org`
     }
 
+    expect(await validateConfig(piGenConfig)).toBeUndefined()
+  })
+
+  it.each([
+    'deb http://de.archive.ubuntu.com/ubuntu jammy main restricted universe multiverse',
+    '#deb-src http://de.archive.ubuntu.com/ubuntu jammy-updates main restricted universe multiverse',
+    'deb [ arch=arm64 ] http://ports.ubuntu.com/ubuntu-ports/ jammy main restricted universe multiverse',
+    'deb-src http://security.debian.org/debian-security stable-security main contrib non-free-firmware'
+  ])('should allow tempRepo with line value %s', async (value: string) => {
+    const piGenConfig = {
+      ...DEFAULT_CONFIG,
+      stageList: ['stage0', 'stage1', 'stage2'],
+      tempRepo: value
+    }
     expect(await validateConfig(piGenConfig)).toBeUndefined()
   })
 })
